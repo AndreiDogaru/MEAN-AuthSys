c7ee9d0455766a9abbc0f6eed812424bf30acf49
Andrei Dogaru <doggy@Andreis-MacBook-Pro.local>
2017-04-25 14:29:29 +0200

    backend api v1.0

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
+const config = require("../config/database");
 
 router.post('/register', (req,res) => {
     let newUser = new User ({
@@ -22,11 +23,45 @@ router.post('/register', (req,res) => {
 });
 
 router.post('/authenticate', (req,res) => {
-    res.send('Authenticate');
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.getUserByUsername(username, (err, user) => { // check for user
+        if(err) throw err;
+
+        if(!user){ // no match
+            return res.json({success: false, msg: "User not found."});
+        }
+
+        User.comparePassword(password, user.password, (err, isMatch) => { // if user exists, check for password
+            if(err) throw err;
+
+            if(isMatch){
+                const token = jwt.sign(user, config.secret, { // sign the user in
+                    expiresIn: 604800 // keep him sign in for 1 week (in seconds)
+                });
+
+                res.json({ // send the user to the client side
+                    success: true,
+                    token: "JWT "+token,
+                    user: {
+                        id: user._id,
+                        name: user.name,
+                        username: user.username,
+                        email: user.email
+                    }
+                });
+            }else{ // no match
+                return res.json({success: false, msg: "Wrong password."});
+            }
+        });
+    });
 });
 
-router.get('/profile', (req,res) => {
-    res.send('Profile');
+router.get('/profile', passport.authenticate('jwt', {session: false}), (req,res) => {
+    res.json({
+        user: req.user
+    });
 });
 
 module.exports = router;
