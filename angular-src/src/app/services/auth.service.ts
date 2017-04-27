bcf4b45237e44f5d5878e23b826095e0b21d96ce
Andrei Dogaru <doggy@Andreis-MacBook-Pro.local>
2017-04-27 21:21:02 +0200

    angular2 login + logout

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -16,4 +16,24 @@ export class AuthService {
                     .map((res:Response) => res.json());
   }
 
+  authenticateUser(user){
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers})
+                    .map((res:Response) => res.json());
+  }
+
+  storeUserData(token,user){
+    localStorage.setItem('id_token',token);
+    localStorage.setItem('user',JSON.stringify(user)); // localStorage can only store strings, not Objects
+    this.authToken = token;
+    this.user = user;
+  }
+
+  logout(){
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
+  }
+
 }
